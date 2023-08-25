c6c0b96e6e7aa4626b18f044ee009402b2058afb
moezmissaoui <moeiz.missaoui@gmail.com>
2023-08-25 20:01:44 +0100

    implement pagination for data

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -3,7 +3,9 @@ namespace App\Traits;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Response;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Validator;
 
 trait ApiResponser
 {
@@ -29,6 +31,7 @@ trait ApiResponser
         $transformer = $collection->first()->transformer;
         $collection = $this->filterData($collection, $transformer);
         $collection = $this->sortData($collection, $transformer);
+        $collection = $this->paginate($collection);
         $collection = $this->transformData($collection, $transformer);
         return $this->successResponse($collection, $code);
     }
@@ -63,6 +66,33 @@ trait ApiResponser
             $collection = $collection->sortBy->{$attribute};
         }
         return $collection;
+    }
+
+    protected function paginate(Collection $collection) 
+    {
+        $rules = [
+            'per_page' => 'integer|min:2|max:50'
+        ];
+
+        Validator::validate(request()->all(), $rules);
+
+        $page = LengthAwarePaginator::resolveCurrentPage();
+        $perPage = 15;
+        if (request()->has('per_page')) {
+            $perPage = (int)request()->per_page;
+        }
+        $results = $collection->slice(($page-1) * $perPage, $perPage)->values();
+        $paginated = new LengthAwarePaginator(
+                            $results, 
+                            $collection->count(), 
+                            $perPage, 
+                            $page, 
+                            [
+                                'path' => LengthAwarePaginator::resolveCurrentPath()
+                            ]
+                    );
+        $paginated->appends(request()->all());
+        return $paginated;
     }
 
     protected function transformData($data, $transformer) 
