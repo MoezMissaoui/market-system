417aafb093e3fe60e8629921fe8609c758e7421b
moezmissaoui <moeiz.missaoui@gmail.com>
2023-08-26 11:01:35 +0100

    transform errors response

--- app/Http/Middleware/TransformInput.php
+++ app/Http/Middleware/TransformInput.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class TransformInput
 {
@@ -21,6 +22,20 @@ class TransformInput
             $transformedInput[$transformer::originalAttribute($input)] = $value;
         }
         $request->replace($transformedInput);
-        return $next($request);
+
+        $response = $next($request);
+
+        if (isset($response->exception) && $response->exception instanceof ValidationException) {
+            $data = $response->getData();
+            $transformedErrors = [];
+            foreach ($data->error as $field => $error) {
+                $transformedField = $transformer::transformedAttribute($field);
+                $transformedErrors[$transformedField] = str_replace($field, $transformedField, $error);
+            }
+            $data->error = $transformedErrors;
+            $response->setData($data);
+        }
+
+        return $response;
     }
 }
